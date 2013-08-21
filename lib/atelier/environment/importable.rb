261c1e0548d822cc4832ea92be12c794d4d03a68
pasberth <pasberth@gmail.com>
2013-08-21 12:14:26 +0900

    Module.new より self.clone したほうがいろいろと行儀良いね。

--- lib/atelier/environment/importable.rb
+++ lib/atelier/environment/importable.rb
@@ -2,20 +2,24 @@ module Atelier; module Environment
 
   module Importable
 
-    def [](aliases); m = self
-      Module.new  do; include m; extend Importable
-        aliases.each do |x, y|
-          alias_method y, x
-          undef_method x
-        end
-      end
+    def [](aliases)
+      self.clone.tap do |m|
+        m.module_eval do
+          aliases.each do |x, y|
+            alias_method y, x
+            undef_method x
+          end; end; end
     end
 
     def only(*names)
       hiding *instance_methods + private_instance_methods - names
     end
 
-    def hiding(*names); m = self
-      Module.new { include m; names.each {|x| undef_method x }}
+    def hiding(*names)
+      self.clone.tap do |m|
+        m.module_eval do
+          names.each do |x|
+            undef_method x
+          end; end; end
     end
   end; end; end
